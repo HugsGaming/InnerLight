ad7fab9c9e95143e57511e69ecd906269cf10b90
HugsGaming <jovenreyes020803@gmail.com>
2024-06-08 22:58:32 +0800

    feat: add markdown rendering for the chats between users and chatbot

--- InnerLightWebsite/app/aiTest/page.tsx
+++ InnerLightWebsite/app/aiTest/page.tsx
@@ -1,23 +1,25 @@
-'use client'
+"use client";
 
-import { type Message, useAssistant } from 'ai/react'
-import React from 'react'
+import { type Message, useAssistant } from "ai/react";
+import React from "react";
+import Markdown from "react-markdown";
 
 export default function Page() {
-  const {status, messages, input, submitMessage, handleInputChange} = useAssistant({
-    api: '/api/chatbot'
-  })
+  const { status, messages, input, submitMessage, handleInputChange } =
+    useAssistant({
+      api: "/api/chatbot",
+    });
   return (
-    <div className=''>
-      {messages.map((m : Message) => (
-        <div key={m.id} className='flex flex-col gap-1 border-b p-2'>
+    <div className="">
+      {messages.map((m: Message) => (
+        <div key={m.id} className="flex flex-col gap-1 border-b p-2">
           <strong>{`${m.role}: `}</strong>
-          {m.role !== 'data' && m.content}
-          {m.role === 'data' && (
+          {m.role !== "data" && <Markdown>{m.content}</Markdown>}
+          {m.role === "data" && (
             <>
               {(m.data as any).description}
               <br />
-              <pre className='bg-amber-300'>
+              <pre className="bg-amber-300">
                 {JSON.stringify(m.data, null, 2)}
               </pre>
             </>
@@ -25,17 +27,20 @@ export default function Page() {
         </div>
       ))}
 
-      {status === 'in_progress' && <div />}
-      
-      <form onSubmit={submitMessage} className='flex flex-row gap-2 p-2 bg-zinc-100 w-full'>
+      {status === "in_progress" && <div />}
+
+      <form
+        onSubmit={submitMessage}
+        className="flex flex-row gap-2 p-2 bg-zinc-100 w-full"
+      >
         <input
-          className='bg-zinc-100 w-full p-2 outline-none'
-          disabled={status != 'awaiting_message'}
+          className="bg-zinc-100 w-full p-2 outline-none"
+          disabled={status != "awaiting_message"}
           value={input}
-          placeholder='Type a message'
+          placeholder="Type a message"
           onChange={handleInputChange}
         />
       </form>
     </div>
-  )
+  );
 }
