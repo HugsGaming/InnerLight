e612845ab1e07c7c1b292d315a11ba75583a1095
HugsGaming <jovenreyes020803@gmail.com>
2024-06-08 19:40:57 +0800

    feat: add ai_chatbot

---
+++ InnerLightWebsite/app/aiTest/layout.tsx
@@ -0,0 +1,6 @@
+import { type ReactNode } from "react";
+import { AI } from "../actions";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return <AI>{children}</AI>;
+}
