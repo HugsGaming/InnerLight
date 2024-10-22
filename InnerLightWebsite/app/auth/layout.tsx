37056e305d3a0fffb94a0be73ff28ca8be5c86d7
HugsGaming <jovenreyes020803@gmail.com>
2024-10-22 19:42:24 +0800

    build: remove next-recaptcha

--- InnerLightWebsite/app/auth/layout.tsx
+++ InnerLightWebsite/app/auth/layout.tsx
@@ -1,17 +1,11 @@
 import { type ReactNode } from "react";
-import { ReCaptchaProvider } from "next-recaptcha-v3";
 import "../globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="en">
             <body>
-                <ReCaptchaProvider
-                    reCaptchaKey={process.env.RECAPTCHA_SITE_KEY}
-                    useEnterprise
-                >
-                    {children}
-                </ReCaptchaProvider>
+                {children}
             </body>
         </html>
     );
