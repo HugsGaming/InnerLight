0725ae3c2746fc270ad04b7bb2dfcf78568f33dd
HugsGaming <jovenreyes020803@gmail.com>
2024-11-01 22:27:15 +0800

    feat: fetch profile username for post

--- InnerLightWebsite/app/home/page.tsx
+++ InnerLightWebsite/app/home/page.tsx
@@ -7,68 +7,84 @@ import Post from "../components/Post";
 import { createClient } from "../utils/supabase/server";
 import { redirect } from "next/navigation";
 
-const Home: React.FC = () => {
+const Home: React.FC = async () => {
     const supabase = createClient();
-
+    let usernameData;
     supabase.auth.getUser().then(async ({ data }) => {
         const { user } = data;
         if (!user || user === null) {
             redirect("/auth/login");
         } else {
-            let metadata = user.user_metadata;
-            console.log(metadata);
-            const first_name = metadata.first_name;
-            const last_name = metadata.last_name;
-            const username = metadata.username;
-            await supabase
-                .from("profiles")
-                .select("*")
-                .eq("id", user.id)
-                .single()
-                .then(async ({ data }) => {
-                    console.log("Profile Data: ", data);
-                    if (!data || data === null) {
-                        try {
-                            console.log(first_name, last_name, username);
-                            const { data: profileData, error } = await supabase
-                                .from("profiles")
+            
+        }
+    });
+    const {data: {user}} = await supabase.auth.getUser();
+    let profile;
+
+    if(!user || user == null) {
+
+    } else {
+        let metadata = user.user_metadata;
+        console.log(metadata);
+        const first_name = metadata.first_name;
+        const last_name = metadata.last_name;
+        const username = metadata.username;
+        await supabase
+            .from("profiles")
+            .select("*")
+            .eq("id", user.id)
+            .single()
+            .then(async ({ data }) => {
+                console.log("Profile Data: ", data);
+                if (!data || data === null) {
+                    try {
+                        console.log(first_name, last_name, username);
+                        const { data: profileData, error } = await supabase
+                            .from("profiles")
+                            .insert({
+                                first_name: first_name,
+                                last_name: last_name,
+                                username: username,
+                                email: user.email,
+                            });
+                        if (error) {
+                            console.log(error);
+                        } else {
+                            console.log("Profile created");
+                            const { data: logData, error } = await supabase
+                                .from("auditLogs")
                                 .insert({
-                                    first_name: first_name,
-                                    last_name: last_name,
-                                    username: username,
-                                    email: user.email,
+                                    user_id: user.id,
+                                    action_type: "Registration",
+                                    add_info: null,
                                 });
                             if (error) {
                                 console.log(error);
                             } else {
-                                console.log("Profile created");
-                                const { data: logData, error } = await supabase
-                                    .from("auditLogs")
-                                    .insert({
-                                        user_id: user.id,
-                                        action_type: "Registration",
-                                        add_info: null,
-                                    });
-                                if (error) {
-                                    console.log(error);
-                                } else {
-                                    console.log("Log created");
-                                }
+                                console.log("Log created");
                             }
-                        } catch (error) {
-                            console.log(error);
                         }
-                    } else {
-                        await supabase.from("auditLogs").insert({
-                            user_id: user.id,
-                            action_type: "Login",
-                            add_info: null,
-                        });
-                        console.log("Log created");
+                    } catch (error) {
+                        console.log(error);
                     }
-                });
-        }
-    });
+                } else {
+                    await supabase.from("auditLogs").insert({
+                        user_id: user.id,
+                        action_type: "Login",
+                        add_info: null,
+                    });
+                    console.log("Log created");
+                }
+            });
+            const {data: profileData, error: profileError} = await supabase.from("profiles").select("*").eq("id", user.id).single();
+            if(profileError || profileData === null) {
+                throw(profileError);
+            }else {
+                profile = profileData;
+            }
+    }
+
+    
 
     return (
         <div
@@ -89,7 +105,7 @@ const Home: React.FC = () => {
                         email: "",
                         first_name: "",
                         last_name: "",
-                        username: "user.username",
+                        username: profile.username!,
                     }}
                 />
             </div>
