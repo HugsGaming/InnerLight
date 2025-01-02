9ace48ee487108f7a2efcceeb14256fb2c857d0a
HugsGaming <jovenreyes020803@gmail.com>
2025-01-02 21:56:41 +0800

    build: made import of component dynamic

--- InnerLightWebsite/app/chatbot/page.tsx
+++ InnerLightWebsite/app/chatbot/page.tsx
@@ -1,11 +1,15 @@
-"use server";
+import { ToastContainer } from "react-toastify";
+import dynamic from "next/dynamic";
 
-import NewEmotionDetectionChat from "../components/new-emotion-detection/NewEmotionDetectionChat";
+const NewEmotionDetectionChat = dynamic(() => import("../components/new-emotion-detection/NewEmotionDetectionChat"), {
+    ssr: false
+})
 
 export default async function Page() {
     return (
         <main className="min-h-screen bg-gray-50 py-8">
             <NewEmotionDetectionChat />
+            <ToastContainer />
         </main>
     );
 }
