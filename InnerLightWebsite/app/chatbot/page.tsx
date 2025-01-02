424d215c8407cadb477881ce03055fee2b3ff147
HugsGaming <jovenreyes020803@gmail.com>
2025-01-02 21:36:12 +0800

    build: made chatbot page a server page

--- InnerLightWebsite/app/chatbot/page.tsx
+++ InnerLightWebsite/app/chatbot/page.tsx
@@ -1,6 +1,8 @@
+"use server";
+
 import NewEmotionDetectionChat from "../components/new-emotion-detection/NewEmotionDetectionChat";
 
-export default function Page() {
+export default async function Page() {
     return (
         <main className="min-h-screen bg-gray-50 py-8">
             <NewEmotionDetectionChat />
